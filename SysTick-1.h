22596327f1edebb1c88aad248705fd4cfa8061c5
Abdelrahman Sameh <104277222+Abdelrahman-Samehh@users.noreply.github.com>
2022-05-06 15:55:27 +0200

    Add files via upload

---
+++ SysTick-1.h
@@ -0,0 +1,11 @@
+#ifndef SYSTICK_H_
+#define SYSTICK_H_
+#include "tm4c123gh6pm.h"
+
+// Functions definitions of SysTick
+void SysTick_Init(void);
+void SysTick_Wait(uint32_t delay);
+void SysTick_Wait1ms(uint32_t delayms);
+void SysTick_Wait1us(uint32_t delayus)
+
+#endif // !SYSTICK_H_
