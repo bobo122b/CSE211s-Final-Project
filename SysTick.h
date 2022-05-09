8959755f51aaf6ddbf033ccd4a3a5e9f30f5298b
Abdelrahman Mostafa <boboelahlawy1234@hotmail.com>
2022-05-09 19:49:05 +0200

    Adding PLL function to SysTick source file

--- SysTick.h
+++ SysTick.h
@@ -6,6 +6,7 @@
 void SysTick_Init(void);
 void SysTick_Wait(uint32_t delay);
 void SysTick_Wait1ms(uint32_t delayms);
-void SysTick_Wait1us(uint32_t delayus)
+void SysTick_Wait1us(uint32_t delayus);
+void PLL_Init(void);
 
 #endif // !SYSTICK_H_
